6a70baa5eb09b22195499b48a8ca9a7057c36d11
Richard Zou <richardzou24@gmail.com>
2024-04-04 17:08:41 -0400

    built functions getComputerChoice() and playRound() and linked script.js to html file.

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 function getComputerChoice() {
-
+    let choices = ["rock", "paper", "scissors"];
+    let randomIndex = [Math.floor(Math.random * choices.length)];
+    return randomIndex;
 }
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
 
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return "You win!"
+    } else {
+        return "You lose!"
+    }
 }
 
 function playGame() {
